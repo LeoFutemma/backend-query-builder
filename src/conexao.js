3cb6d39e59238e363cb4ec63753bbb901d7134c6
LeoFutemma <lftumma@gmail.com>
2022-07-14 19:25:15 -0300

    adiciona variavel de ambiente

--- src/conexao.js
+++ src/conexao.js
@@ -1,21 +1,11 @@
-// const knex = require('knex')({
-//     client: 'pg',
-//     connection: {
-//         host: 'localhost',
-//         user: 'postgres',
-//         password: '123456',
-//         database: 'postgres',
-//         port: 5432
-//     }
-// });
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: 'ec2-3-219-229-143.compute-1.amazonaws.com',
-        user: 'poegcmifbgehkl',
-        password: 'c5c0599e5d4a177ad2368e3bcf8bcfa6baeddcf155f4092c19ba4aa7219a0da7',
-        database: 'd3k57bhif9qn7l',
-        port: 5432,
+        host: process.env.DB_HOST || 'ec2-3-219-229-143.compute-1.amazonaws.com',
+        user: process.env.DB_USER || 'poegcmifbgehkl',
+        password: process.env.DB_PASS || 'c5c0599e5d4a177ad2368e3bcf8bcfa6baeddcf155f4092c19ba4aa7219a0da7',
+        database: process.env.DB_NAME || 'd3k57bhif9qn7l',
+        port: process.env.DB_PORT || 5432,
         ssl: {
             rejectUnauthorized: false
         }
