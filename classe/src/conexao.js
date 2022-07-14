9fff466caf8a72958f79b38cc0f2ee3061c7f5a2
LeoFutemma <lftumma@gmail.com>
2022-07-14 19:13:01 -0300

    adiciona start nos scripts

--- classe/src/conexao.js
+++ classe/src/conexao.js
@@ -1,3 +1,13 @@
+// const knex = require('knex')({
+//     client: 'pg',
+//     connection: {
+//         host: 'localhost',
+//         user: 'postgres',
+//         password: '123456',
+//         database: 'postgres',
+//         port: 5432
+//     }
+// });
 const knex = require('knex')({
     client: 'pg',
     connection: {
